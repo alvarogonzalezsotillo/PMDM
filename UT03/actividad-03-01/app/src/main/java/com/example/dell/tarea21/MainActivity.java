b775585d7ce81e0d2bbea5f79c15f9beffc9fcc4
Álvaro González <alvarogonzalezsotillo@gmail.com>
2021-09-16 21:39:18 +0200

    3-1 ya compila y se ejecuta

---
+++ UT03/actividad-03-01/app/src/main/java/com/example/dell/tarea21/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dell.tarea21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etOperando1, etOperando2;
+    TextView tvResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etOperando1=(EditText)findViewById(R.id.etOperando1);
+        etOperando2=(EditText)findViewById(R.id.etOperando2);
+        tvResultado=(TextView)findViewById(R.id.tvResultado);
+    }
+
+    public void suma(View v){
+        int num1, num2;
+
+        try{
+            num1=Integer.parseInt(etOperando1.getText().toString());
+            num2=Integer.parseInt(etOperando2.getText().toString());
+            tvResultado.setText(""+(num1+num2));
+        }catch(NumberFormatException e){
+            tvResultado.setText("Los números deben ser enteros");
+        }
+    }
+
+
+}
