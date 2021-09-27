5f3f6547d541d4eb0d4f1fd286f3c8c5000b739f
alvaro@thinkpad <alvaro@thinkpad>
2021-09-27 16:41:59 +0200

    añadido 03-02

---
+++ UT03/actividad-03-02/app/src/main/java/com/example/dell/tarea32/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dell.tarea32;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etOperando1, etOperando2;
+    TextView tvResultado;
+    Button btnSumar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etOperando1 = (EditText) findViewById(R.id.etOperando1);
+        etOperando2 = (EditText) findViewById(R.id.etOperando2);
+        tvResultado = (TextView) findViewById(R.id.tvResultado);
+        btnSumar = (Button) findViewById(R.id.btnSumar);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1, num2;
+
+                try{
+                    num1=Integer.parseInt(etOperando1.getText().toString());
+                    num2=Integer.parseInt(etOperando2.getText().toString());
+                    tvResultado.setText(""+(num1+num2));
+                }catch(NumberFormatException e){
+                    tvResultado.setText("Los números deben ser enteros");
+                }
+            }
+        });
+    }
+}
