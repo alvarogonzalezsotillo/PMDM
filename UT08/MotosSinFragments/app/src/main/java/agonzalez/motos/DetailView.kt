b8e51aa61d9a13ec6d1cf72c9094e408952043e2
alvaro@thinkpad <alvaro@thinkpad>
2022-01-16 18:39:15 +0100

    Mejor !!

--- UT08/MotosSinFragments/app/src/main/java/agonzalez/motos/DetailView.kt
+++ UT08/MotosSinFragments/app/src/main/java/agonzalez/motos/DetailView.kt
@@ -10,28 +10,28 @@ class DetailView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_view)
-        val tvMarca = findViewById<TextView>(R.id.tvMarca)
-        val tvDescripcion = findViewById<TextView>(R.id.tvDescripcion)
-        val imgvLogo = findViewById<ImageView>(R.id.imgvLogo)
-        val imgvFoto = findViewById<ImageView>(R.id.imgvFoto)
-        val bundle = intent.extras
-        val modelo = bundle!!.getInt("modelo")
+        val tvMarca = findViewById<TextView>(R.id.tvMarca)!!
+        val tvDescripcion = findViewById<TextView>(R.id.tvDescripcion)!!
+        val imgvLogo = findViewById<ImageView>(R.id.imgvLogo)!!
+        val imgvFoto = findViewById<ImageView>(R.id.imgvFoto)!!
+        val bundle = intent.extras!!
+        val modelo = bundle.getInt("modelo")
         assert(modelo in 0..2)
         when (modelo) {
             0 -> {
-                imgvLogo!!.setImageResource(R.drawable.logo_bultaco)
-                imgvFoto!!.setImageResource(R.drawable.imagen_bultaco)
+                imgvLogo.setImageResource(R.drawable.logo_bultaco)
+                imgvFoto.setImageResource(R.drawable.imagen_bultaco)
             }
             1 -> {
-                imgvLogo!!.setImageResource(R.drawable.logo_harley_davidson)
-                imgvFoto!!.setImageResource(R.drawable.imagen_harley)
+                imgvLogo.setImageResource(R.drawable.logo_harley_davidson)
+                imgvFoto.setImageResource(R.drawable.imagen_harley)
             }
             2 -> {
-                imgvLogo!!.setImageResource(R.drawable.logo_moto_guzzi)
-                imgvFoto!!.setImageResource(R.drawable.imagen_moto_guzzi)
+                imgvLogo.setImageResource(R.drawable.logo_moto_guzzi)
+                imgvFoto.setImageResource(R.drawable.imagen_moto_guzzi)
             }
         }
-        tvMarca!!.text = resources.getStringArray(R.array.nombre)[modelo]
-        tvDescripcion!!.text = resources.getStringArray(R.array.descripcion)[modelo]
+        tvMarca.text = resources.getStringArray(R.array.nombre)[modelo]
+        tvDescripcion.text = resources.getStringArray(R.array.descripcion)[modelo]
     }
 }
